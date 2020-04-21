8221401d502fa7b4e7ac722d43598a0bb1ddfd8b
Raphael Schindler <r.schindler@dgfrp.de>
2020-04-21 10:59:51 +0200

    Update myscript.js

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,4 +1,4 @@
-document.addEventListener("load", function() {
+window.addEventListener("load", function() {
 	document.body.style.backgroundColor = "red";
 	let ele = document.getElementsByTagName("h3");
 	ele[0].innerHTML = "pwned";
